6d0288bde82b3592788b739ee620784be5330984
Angel <emili17_2002@hotmaail.com>
2023-07-14 19:12:06 -0600

    Esp32 ClientWebSocket and websocket server in Python

---
+++ Esp32_Sockets.ino
@@ -0,0 +1,65 @@
+#include <ArduinoWebsockets.h>
+#include <WiFi.h>
+#include <ArduinoJson.h>
+#include <DHT.h>
+
+const char* ssid = "Angel A50"; // Ingresa el SSID
+const char* password = "TZRI4979"; // Ingresa la contraseña
+const char* websockets_server_host = "192.168.100.150"; // Ingresa la dirección del servidor
+const uint16_t websockets_server_port = 5000; // Ingresa el puerto del servidor
+
+#define DHTPIN 27          // Pin del sensor DHT22
+#define DHTTYPE DHT22     // Tipo de sensor DHT (DHT11 o DHT22)
+
+DHT dht(DHTPIN, DHTTYPE,22); // Inicializar el objeto DHT
+
+using namespace websockets;
+
+WebsocketsClient client;
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Conexión a la red WiFi
+  WiFi.begin(ssid, password);
+
+  // Esperar a que se conecte a la red WiFi
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Conectado a la red WiFi");
+
+  // Inicializar el sensor DHT22
+  dht.begin();
+
+  // Conectar al servidor WebSocket
+  Serial.println("Conectando al servidor WebSocket...");
+  if (client.connect(websockets_server_host, websockets_server_port, "/")) {
+    Serial.println("Conexión WebSocket establecida");
+  } else {
+    Serial.println("Error al conectar al servidor WebSocket");
+  }
+}
+
+void loop() {
+  // Leer temperatura y humedad del sensor DHT22
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  // Crear un objeto JSON y asignar los valores de temperatura y humedad
+  DynamicJsonDocument jsonDocument(256);
+  jsonDocument["humidity"] = humidity;
+  jsonDocument["temperature"] = temperature;
+
+  // Serializar el objeto JSON en una cadena
+  String jsonData;
+  serializeJson(jsonDocument, jsonData);
+
+  // Enviar la cadena JSON al servidor WebSocket
+  client.send(jsonData);
+
+  // Esperar un momento antes de enviar el siguiente dato
+  delay(2000);
+}
--- server.py
+++ server.py
@@ -1,36 +1,18 @@
+#!/usr/bin/env python
+
 import asyncio
 import websockets
-import pymongo
-import json
 
-# Configuración de la conexión a MongoDB Atlas
-mongo_url = "mongodb+srv://<usuario>:<contraseña>@<cluster>.mongodb.net/<basededatos>?retryWrites=true&w=majority"
-client = pymongo.MongoClient(mongo_url)
-db = client["nombre_de_la_base_de_datos"]
-collection = db["nombre_de_la_coleccion"]
-
-
-async def handle_websocket(websocket, path):
+async def handle_client(websocket):
     while True:
-        # Recibir datos desde el cliente WebSocket
-        data = await websocket.recv()
-
-        # Parsear el JSON recibido
-        json_data = json.loads(data)
-
-        # Guardar los datos en la colección de MongoDB
-        collection.insert_one(json_data)
-
-        # Enviar una respuesta al cliente WebSocket
-        response = "Datos guardados en MongoDB Atlas"
-        await websocket.send(response)
+        message = await websocket.recv()
+        print("Mensaje recibido del cliente:", message)
 
 
-async def start_server():
-    server = await websockets.serve(handle_websocket, "0.0.0.0", 8080)
-    print("Servidor WebSocket iniciado")
+async def main():                             #Recibira de cualquier direccion
+    async with websockets.serve(handle_client,"0.0.0.0", 5000):
+        print('Se inicio el servidor')
+        await asyncio.Future()  # run forever
 
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(start_server())
-loop.run_forever()
+if __name__ == "__main__":
+    asyncio.run(main())
