48b3d807d10a39c0a7d0a1e9a328a6f9cfda86a7
Angel Gabriel <100612184+ElExca@users.noreply.github.com>
2023-07-17 23:03:14 -0600

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import os
 from reportlab.lib.pagesizes import letter
 import  tempfile
-
+from flask import current_app
 def generar_graficaHumedad(arr_sorted):
     # Crear la gráfica utilizando Matplotlib
     x_labels = range(0, len(arr_sorted), 5)  # Etiquetas del eje x
@@ -91,7 +91,8 @@ def generar_pdf(table_frecuency_campo2,table_frecuency_campo1,table_frecuency_ca
         desviacion_media_campo1, media_campo1, varianza_campo1, desviacion_estandar_campo1,pdf_filename,
         desviacion_media_campo3, media_campo3, varianza_campo3, desviacion_estandar_campo3, arr_ordenate_campo3,
         arr_sorted_campo3,desviacion_media_campo4, media_campo4, varianza_campo4, desviacion_estandar_campo4, arr_ordenate_campo4,
-        arr_sorted_campo4,desviacion_media_campo5, media_campo5, varianza_campo5, desviacion_estandar_campo5,arr_sorted_campo5,arr_ordenate_campo5):
+        arr_sorted_campo4,desviacion_media_campo5, media_campo5, varianza_campo5, desviacion_estandar_campo5,arr_sorted_campo5,arr_ordenate_campo5,moda_campo1,moda_campo2
+                ,moda_campo3,moda_campo4,moda_campo5):
 
     table_frecuency_campo1.to_string(index=False)
     table_frecuency_campo2.to_string(index=False)
@@ -99,9 +100,10 @@ def generar_pdf(table_frecuency_campo2,table_frecuency_campo1,table_frecuency_ca
     table_frecuency_campo4.to_string(index=False)
     table_frecuency_campo5.to_string(index=False)
 
-    download_folder = os.path.join(os.path.expanduser("~"), "Downloads")
-    # Combinar la ruta de descargas con el nombre del archivo PDF
-    pdf_path = os.path.join(download_folder, pdf_filename)
+    public_folder = current_app.static_folder
+    # Generar la ruta completa del archivo PDF en la carpeta 'Public'
+    pdf_filename = 'ReporteSensores.pdf'
+    pdf_path = os.path.join(public_folder, pdf_filename)
     c = canvas.Canvas(pdf_path, pagesize=letter)
     c.setFont("Helvetica", 12)
     c.drawString(70, 700, "Datos de Humedad:")
@@ -141,6 +143,9 @@ def generar_pdf(table_frecuency_campo2,table_frecuency_campo1,table_frecuency_ca
     y_pos -= 40  # Espacio adicional antes de la desviación media
     c.drawString(70, y_pos, "Desviacion estandar humedad:")
     c.drawString(70, y_pos - 20, str(round(desviacion_estandar_campo2,2)))
+    y_pos -= 40  # Espacio adicional antes de la desviación media
+    c.drawString(70, y_pos, "Moda Humedad:")
+    c.drawString(70, y_pos - 20, str(moda_campo2))
     c.showPage()
     temp_filename = generar_graficaHumedad(arr_sorted_campo2)
     c.drawImage(temp_filename, 80, 400, width=500, height=400)
@@ -184,7 +189,7 @@ def generar_pdf(table_frecuency_campo2,table_frecuency_campo1,table_frecuency_ca
         y_start -= line_height
 
     c.showPage()
-
+    #Temperatura
     c.drawString(70, 700, "Datos de Temperatura:")
     # Ajustar la posición de los datos de humedad en forma horizontal con salto de línea
     x_pos = 70
@@ -220,6 +225,9 @@ def generar_pdf(table_frecuency_campo2,table_frecuency_campo1,table_frecuency_ca
     y_pos -= 40  # Espacio adicional antes de la desviación media
     c.drawString(70, y_pos, "Desviacion estandar Temperatura:")
     c.drawString(70, y_pos - 20, str(round(desviacion_estandar_campo1, 2)))
+    y_pos -= 40  # Espacio adicional antes de la desviación media
+    c.drawString(70, y_pos, "Moda Temperatura:")
+    c.drawString(70, y_pos - 20, str(moda_campo1))
     c.showPage()
     temp_filename = generar_graficaTemperature(arr_sorted_campo1)
     c.drawImage(temp_filename, 80, 400, width=500, height=400)
@@ -301,6 +309,9 @@ def generar_pdf(table_frecuency_campo2,table_frecuency_campo1,table_frecuency_ca
     y_pos -= 40  # Espacio adicional antes de la desviación media
     c.drawString(70, y_pos, "Desviacion estandar Presion:")
     c.drawString(70, y_pos - 20, str(round(desviacion_estandar_campo3, 2)))
+    y_pos -= 40  # Espacio adicional antes de la desviación media
+    c.drawString(70, y_pos, "Moda Presion:")
+    c.drawString(70, y_pos - 20, str(moda_campo3))
     c.showPage()
     temp_filename = generar_graficaPressure(arr_sorted_campo3)
     c.drawImage(temp_filename, 90, 400, width=400, height=300)
@@ -347,7 +358,7 @@ def generar_pdf(table_frecuency_campo2,table_frecuency_campo1,table_frecuency_ca
 
     c.showPage()
 
-    #CO2
+    # CO2
     c.drawString(70, 700, "Datos de C02:")
     # Ajustar la posición de los datos de humedad en forma horizontal con salto de línea
     x_pos = 70
@@ -382,6 +393,9 @@ def generar_pdf(table_frecuency_campo2,table_frecuency_campo1,table_frecuency_ca
     y_pos -= 40  # Espacio adicional antes de la desviación media
     c.drawString(70, y_pos, "Desviacion estandar CO2:")
     c.drawString(70, y_pos - 20, str(round(desviacion_estandar_campo4, 2)))
+    y_pos -= 40  # Espacio adicional antes de la desviación media
+    c.drawString(70, y_pos, "Moda CO2:")
+    c.drawString(70, y_pos - 20, str(moda_campo4))
     c.showPage()
     temp_filename = generar_graficaCO2(arr_sorted_campo4)
     c.drawImage(temp_filename, 90, 400, width=400, height=300)
@@ -440,16 +454,23 @@ def generar_pdf(table_frecuency_campo2,table_frecuency_campo1,table_frecuency_ca
             y_pos -= 20  # Saltar a la siguiente línea
         c.drawString(x_pos, y_pos, str(dato))
         x_pos += text_width + 20  # Dejar un espacio entre los datos
-    x_pos = 70
     y_pos -= 40  # Espacio adicional antes de la desviación media
-    c.drawString(70, 560, "Datos ordenados de Altitud:")
-    for dato in arr_ordenate_campo5:
-        text_width = c.stringWidth(str(dato), "Helvetica", 12)
+    y_pos = 560  # Ajustar la posición vertical para comenzar la tabla de datos ordenados de Altitud
+    max_width = 500
+    line_height = 30  # Aumentar el espacio vertical entre líneas
+    font_size = 12  # Aumentar el tamaño de la fuente
+
+    c.setFont("Helvetica", font_size)  # Configurar el tamaño de la fuente
+    arr_sorted_campo5.sort()
+    c.drawString(70, y_pos, "Datos ordenados de Altitud:")
+    for dato in arr_sorted_campo5:
+        dato_redondeado = round(dato, 2)  # Redondear el dato a dos decimales
+        text_width = c.stringWidth(str(dato_redondeado), "Helvetica", font_size)
         if x_pos + text_width > max_width:
             x_pos = 70  # Volver al inicio de la línea
-            y_pos -= 20  # Saltar a la siguiente línea
-        c.drawString(x_pos, y_pos, str(dato))
-        x_pos += text_width + 20  # Dejar un espacio entre los datos
+            y_pos -= line_height  # Saltar a la siguiente línea con el nuevo espacio vertical
+        c.drawString(x_pos, y_pos, str(dato_redondeado))
+        x_pos += text_width + 10  # Reducir el espaciado horizontal entre datos
     # Ajustar la posición vertical después de mostrar los datos de humedad
     y_pos -= 40  # Espacio adicional antes de la desviación media
     c.drawString(70, y_pos, "Desviación Media de Altitud:")
@@ -463,6 +484,9 @@ def generar_pdf(table_frecuency_campo2,table_frecuency_campo1,table_frecuency_ca
     y_pos -= 40  # Espacio adicional antes de la desviación media
     c.drawString(70, y_pos, "Desviacion estandar Altitud:")
     c.drawString(70, y_pos - 20, str(round(desviacion_estandar_campo5, 2)))
+    y_pos -= 40  # Espacio adicional antes de la desviación media
+    c.drawString(70, y_pos, "Moda Altitud:")
+    c.drawString(70, y_pos - 20, str(moda_campo5))
     c.showPage()
     temp_filename = generar_graficaAltitude(arr_sorted_campo5)
     c.drawImage(temp_filename, 90, 400, width=400, height=300)
